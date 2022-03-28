5fede7f418581ad50523730d8e499331ad9c18f0
swastik mund <mundswastik@gmail.com>
2022-03-29 00:07:51 +0530

    2nd commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,108 @@
-import { Axios } from "axios";
+// import { Axios } from "axios";
 import React, {useState} from 'react';
 
 
 function Login() {
 
 
+    // const history = useHistory();
     const [usernameReg, setUernameReg] = useState('');
     const [passwordReg, setPasswordReg] = useState ('');
 
+    // const [submitted, setSubmitted] = useState(false);
+    // const [error, setError] = useState(false);
+    // const [samedataError, setSamedataError] = useState(false);
 
-    const register = () => {
-        Axios.post('http//localhost:3001/signup', {
-        username: usernameReg,
-        password: passwordReg,
-        }).then((response)=>{
-        console.log(response);
-        })
-  }
+    const register = async (e) => {
+        e.preventDefault();
+        
+        const res = await fetch("/login", {
+            method: "POST",
+            headers:{
+            "Content-Type" :"application/json"
+            },
+            body: JSON.stringify({
+            username: usernameReg,
+            password: passwordReg,
+            })
+        });
+        const data = await res.json();
+        
+        if  (usernameReg === '' || passwordReg === '') {
+            // setError(true);
+            window.alert("invalid login");
+            console.log("invalid login")
+            }
+        else if(res.status === 404 || !data){
+            // setSamedataError(true);
+            // setSubmitted(false);
+            // setError(false);
+            window.alert("invalid login");
+            console.log("invalid login")
+        } else{
+            // setSubmitted(true);
+            // setError(false);
+            window.alert("succesful login")
+            console.log("successful login")
+
+            // history.push('/');
+
+        }
+    
+    };
+
+
+    // // Showing success message
+    // const successMessage = () => {
+    // return (
+    // <div
+    // className="success"
+    // style={{
+    // display: submitted ? '' : 'none',
+    // }}>
+    // <h1>User {usernameReg} successfully registered!!</h1>
+    // </div>
+    // );
+    // };
+    
+    // Showing error message if error is true
+    // const errorMessage = () => {
+    // return (
+    // <div
+    // className="error"
+    // style={{
+    // display: error ? '' : 'none',
+    // }}>
+    // <h1>Please enter all the fields</h1>
+    // </div>
+    // );
+    // };
+
+    // // Showing samedataerror message if error is true
+    // const samedataErrorMessage = () => {
+    //     return (
+    //     <div
+    //     className="samedataerror"
+    //     style={{
+    //     display: samedataError ? '' : 'none',
+    //     }}>
+    //     <h1>User Already Exists  Please Enter Valid </h1>
+    //     </div>
+    //     );
+    //     };
 
 
 
     return (
         <div class="login">
 			<h1>Login</h1>
-			<form action="/auth" method="post">
+            {/* Calling to the methods
+            <div className="messages">
+            {errorMessage()}
+            {successMessage()}
+            {samedataErrorMessage()}
+            </div> */}
+			<form  method="post">
 				<label for="username">
 					<i class="fas fa-user"></i>
 				</label>
